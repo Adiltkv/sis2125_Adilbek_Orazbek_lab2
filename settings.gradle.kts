db2a54331a3ee2cfa976400909f6b03216c3da18
Adiltkv <100503447+Adiltkv@users.noreply.github.com>
2024-02-17 21:39:32 +0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "sis2125_Adilbek_Orazbek_lab2"
+include(":app")
+ 
